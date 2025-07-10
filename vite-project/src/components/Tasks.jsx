ba44befe35345a188288d9abab8db0062f735b01
jeansf26 <jeansf26>
2025-07-10 14:31:05 -0300

    Funcionando sem local storage

--- vite-project/src/components/Tasks.jsx
+++ vite-project/src/components/Tasks.jsx
@@ -1,6 +1,19 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function tasks(props) {
+  const navigate = useNavigate();
+
+  function seeDetails(task) {
+    
+    //Método seguro para navegar para a página de detalhes da tarefa
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+
+  }
+
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map(function (task) {
@@ -8,13 +21,25 @@ function tasks(props) {
           <li key={task.id} className="flex gap-2">
             <button
               onClick={() => props.onTaskClick(task.id)}
-              className= {' text-left bg-slate-400 text-white p-2 rounded-md w-full' + (task.completed ? ' line-through' : '')}>
+              className={
+                " text-left bg-slate-400 text-white p-2 rounded-md w-full" +
+                (task.completed ? " line-through" : "")
+              }
+            >
               {task.title}
             </button>
-            <button className="bg-slate-400 text-white p-2 rounded-md">
+
+            <button
+              onClick={() => seeDetails(task)}
+              className="bg-slate-400 text-white p-2 rounded-md"
+            >
               <ChevronRightIcon />
             </button>
-            <button onClick={() => props.deleteTask(task.id)} className="bg-slate-400 text-white p-2 rounded-md">
+
+            <button
+              onClick={() => props.deleteTask(task.id)}
+              className="bg-slate-400 text-white p-2 rounded-md"
+            >
               <TrashIcon />
             </button>
           </li>
